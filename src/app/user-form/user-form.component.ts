2e1de669db7a01d201e0316f0061f8b1115de129
DanGichure <gichuredan29@gmail.com>
2024-08-21 13:23:12 +0300

    User form implementation

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrl: './user-form.component.css'
+  styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent {
+  isDropdownOpen = false;
+  user: User = {
+    name: '',
+    email: '',
+    phone: '',
+    address: {
+      city: '',
+      postalCode: '',
+      county: '',
+      country: ''
+    },
+    occupation: '',
+    age: null,
+    company: '',
+    skills: [],
+    gender: 'Male',
+    county: '',
+    biography: '',
+    interests: [],
+    verified: false,
+    id: '',
+    createdAt: new Date,
+    avatar: ''
+  };
 
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  @Output() userCreated = new EventEmitter<User>();
+
+  onSubmit(): void {
+    this.userCreated.emit(this.user);
+    this.resetForm();
+    this.isDropdownOpen = false;
+  }
+
+  private resetForm(): void {
+    this.user = {
+      name: '',
+    email: '',
+    phone: '',
+    address: {
+      city: '',
+      postalCode: '',
+      county: '',
+      country: ''
+    },
+    occupation: '',
+    age: null,
+    company: '',
+    skills: [],
+    gender: 'Male',
+    county: '',
+    biography: '',
+    interests: [],
+    verified: false,
+    id: '',
+    createdAt: new Date,
+    avatar: ''
+    };
+  }
 }
