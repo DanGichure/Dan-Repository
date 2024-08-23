08dfacc10351cef6b42b2938e4d267f8539919c2
DanGichure <gichuredan29@gmail.com>
2024-08-23 14:02:21 +0300

    grid layout implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserListComponent } from './user-list/user-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserManagementComponent } from './user-management/user-management.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'users/:id', component: UserDetailsComponent },
   { path: 'users/verified', component: UserListComponent },
   { path: 'users/unverified', component: UserListComponent },
+  { path: 'users/:id', component: UserDetailsComponent },
   { path: 'users', component: UserManagementComponent},
 ];
 
